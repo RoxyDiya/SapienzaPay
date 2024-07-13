464883f11403124e34feec31d3e0aca9783b92fe
Elena Martellucci <martellucci.1988602@studenti.uniroma1.it>
2024-07-13 22:29:47 +0200

    Turned into Cupertino

--- lib/detailspage.dart
+++ lib/detailspage.dart
@@ -4,6 +4,10 @@ import 'home_page.dart';
 import 'deadlines.dart';
 import 'profile_stud.dart';
 import 'utils.dart';
+import 'package:flutter/cupertino.dart';
+import 'home_page.dart';
+import 'deadlines.dart';
+import 'profile_stud.dart'; // Assuming profile_stud.dart contains ProfileScreen
 
 class DeadlineDetailsPage extends StatefulWidget {
   final Map<String, String> deadline;
@@ -11,14 +15,27 @@ class DeadlineDetailsPage extends StatefulWidget {
   const DeadlineDetailsPage({super.key, required this.deadline});
 
   @override
-  _DeadlineDetailsState createState() => _DeadlineDetailsState();
+  _DeadlineDetailsPageState createState() => _DeadlineDetailsPageState();
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: const Color.fromARGB(255, 111, 20, 28),
+      ),
+      home: const HomePage(),
+    );
+  }
 }
 
-class _DeadlineDetailsState extends State<DeadlineDetailsPage> {
+class _DeadlineDetailsPageState extends State<DeadlineDetailsPage> {
   int _selectedIndex = 1;
 
   final List<Widget> _widgetOptions = <Widget>[
-    HomeScreen(),
+    HomePage(),
     DeadlinesPage(),
     ProfileScreen(),
   ];
@@ -29,148 +46,145 @@ class _DeadlineDetailsState extends State<DeadlineDetailsPage> {
     });
   }
 
-  
-@override
+  @override
   Widget build(BuildContext context) {
-    TextStyle labelStyle = TextStyle(fontSize: 14, fontWeight: FontWeight.w600);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 130, 36, 61).withOpacity(0.7),
-        leading: IconButton(
-          icon: Icon(CupertinoIcons.chevron_left),
-          onPressed: () => Navigator.of(context).pop(),
-          color: Colors.black,
-          
-        ),
-         title: Row(
-          children: [
-            Text(
-              "Deadlines",
-              style: TextStyle(color: Colors.black),
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              color: Color.fromARGB(255, 130, 36, 61).withOpacity(0.7),
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Center(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: NetworkImage('https://via.placeholder.com/100'),
+    return CupertinoPageScaffold(
+      child: Stack(
+        children: [
+          Container(
+            color: Color.fromARGB(255, 130, 36, 61),
+            height: MediaQuery.of(context).padding.top + 44, // height of status bar + navigation bar
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  color: Color.fromARGB(255, 130, 36, 61),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 40,
+                      backgroundImage: NetworkImage('https://via.placeholder.com/100'),
+                    ),
+                  ),
                 ),
-              ),
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              '1° Tuition Fee',
+                              style: TextStyle(
+                                fontSize: 26,
+                                color: Color.fromARGB(255, 111, 20, 28),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'A.A. Year: 23/24',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                              'Status: Unpaid',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Divider(color: CupertinoColors.inactiveGray, thickness: 0.5),
+                      SizedBox(height: 10),
+                      _buildFeeRow('Regional Tax', '€140'),
+                      _buildFeeRow('Postage Stamp', '€16'),
+                      _buildFeeRow('First Installment', '€550'),
+                      _buildFeeRow('Penalty Fee', '€60'),
+                      SizedBox(height: 20),
+                      Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              'DUE ON 15/11/2023',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 111, 20, 28),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                              ),
+                            ),
+                            Text(
+                              '(Expired)',
+                              style: TextStyle(
+                                color: CupertinoColors.inactiveGray,
+                                fontSize: 22,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Center(
+                        child: Text(
+                          '€766',
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 111, 20, 28),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Center(
+                        child: CupertinoButton.filled(
+                          onPressed: () {},
+                          child: const Text(
+                            'Pay Now',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                CupertinoTabBar(
+                  items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(icon: Icon(CupertinoIcons.house_fill), label: 'Home'),
+                    BottomNavigationBarItem(icon: Icon(CupertinoIcons.clock_fill), label: 'Deadlines'),
+                    BottomNavigationBarItem(icon: Icon(CupertinoIcons.person), label: 'Profile'),
+                  ],
+                  currentIndex: _selectedIndex,
+                  activeColor: Color.fromARGB(255, 130, 36, 51),
+                  inactiveColor: Colors.grey,
+                  onTap: _onItemTapped,
+                ),
+              ],
             ),
-            Container(
+          ),
+          CupertinoNavigationBar(
+            leading: CupertinoNavigationBarBackButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              previousPageTitle: 'Deadlines',
               color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Column(
-                      children: [
-                        Text(
-                          '1° Tuition Fee',
-                          style: TextStyle(
-                            fontSize: 26,
-                            color: Color.fromARGB(255, 111, 20, 28),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          'A.A. Year: 23/24',
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                        ),
-                        Text(
-                          'Status: Unpaid',
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Divider(color: CupertinoColors.inactiveGray, thickness: 0.5),
-                  SizedBox(height: 10),
-                  _buildFeeRow('Regional Tax', '€140'),
-                  _buildFeeRow('Postage Stamp', '€16'),
-                  _buildFeeRow('First Installment', '€550'),
-                  _buildFeeRow('Penalty Fee', '€60'),
-                  SizedBox(height: 20),
-                  Center(
-                    child: Column(
-                      children: [
-                        Text(
-                          'DUE ON 15/11/2023',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 111, 20, 28),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 22,
-                          ),
-                        ),
-                        Text(
-                          '(Expired)',
-                          style: TextStyle(
-                            color: CupertinoColors.inactiveGray,
-                            fontSize: 22,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Center(
-                    child: Text(
-                      '€766',
-                      style: TextStyle(
-                        fontSize: 36,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 111, 20, 28),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Pay Now',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 111, 20, 28),
-                        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
-                        textStyle: const TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
             ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.house_fill), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.clock_fill), label: 'Deadlines'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+            backgroundColor: Color.fromARGB(255, 130, 36, 61).withOpacity(0.7),
+            border: Border(
+              bottom: BorderSide.none, // Remove bottom shadow
+            ),
+          ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(255, 130, 36, 51),
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
-        selectedLabelStyle: labelStyle,
-        unselectedLabelStyle: labelStyle.copyWith(color: Colors.grey),
-        iconSize: 25,
       ),
     );
   }
@@ -183,14 +197,44 @@ class _DeadlineDetailsState extends State<DeadlineDetailsPage> {
         children: [
           Text(
             label,
-            style: TextStyle(fontSize: 18),
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 111, 20, 28)),
           ),
           Text(
             value,
-            style: TextStyle(fontSize: 18),
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 111, 20, 28)),
           ),
         ],
       ),
     );
   }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Home Screen"));
+  }
+}
+
+class DeadlinesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Deadlines Page"));
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Profile Screen"));
+  }
+}
+
+void main() {
+  runApp(CupertinoApp(
+    theme: CupertinoThemeData(
+      primaryColor: Color.fromARGB(255, 111, 20, 28),
+    ),
+    home: HomeScreen(),
+  ));
 }
